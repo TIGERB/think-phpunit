1e50d1b0f847802da699ed336c33788e7de181d3
tigerb <624661874@qq.com>
2017-03-14 22:59:19 +0800

    feat: add TestCase.php :bookmark:

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # think-phpunit
-Thinkphp5 phpunit测试用例 
+
+ThinkPHP5——phpunit测试用例
+
+> 由于think官方版的think-testing依赖的phpunit版本太老的问题, 自己写了个thinkphp5下的phpunit测试用例
+
+## 使用
+
+- copy phpunit.xml和tests文件夹到项目根目录
+- composer require phpunit/phpunit
+
+## 版本
+
+- thinkphp 5+
+- phpunit 5+
+
+## 手册
+
+> [phpunit断言API](https://phpunit.de/manual/current/zh_cn/appendixes.assertions.html)
---
+++ tests/TestCase.php
@@ -0,0 +1,16 @@
+<?php
+// 初始化app
+define('ROOT_PATH', $_SERVER['PWD'] . '/');
+define('APP_PATH', $_SERVER['PWD'] . '/application/');
+require __DIR__ . '/../thinkphp/base.php';
+require __DIR__ . '/../thinkphp/helper.php';
+
+use think\Loader;
+
+// 注册命名空间
+Loader::addNamespace('app', __DIR__ . '/../application');
+
+class TestCase extends PHPUnit\Framework\TestCase
+{
+  
+}
